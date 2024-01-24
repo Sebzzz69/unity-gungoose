b26a80c4519989e0e73aad217e6f36010085f4c6
Sebzzz69 <callenholmljungsebastian@gmail.com>
2024-01-24 15:55:27 +0100

    small movement tweaks

--- gungoose/Assets/Scripts/MovementScript.cs
+++ gungoose/Assets/Scripts/MovementScript.cs
@@ -8,6 +8,11 @@ public class MovementScript : MonoBehaviour
     float horizontalInput;
     float verticalInput;
 
+    [SerializeField] KeyCode keyUp;
+    [SerializeField] KeyCode keyDown;
+    [SerializeField] KeyCode keyLeft;
+    [SerializeField] KeyCode keyRight;
+
     Rigidbody2D rb;
 
     Vector2 movement;
@@ -18,37 +23,42 @@ public class MovementScript : MonoBehaviour
     }
     private void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        GetPlayerInput();
 
-        RotatePlayer(horizontalInput, verticalInput);
-        movement = new Vector2(horizontalInput, verticalInput);
+        movement = new Vector2(horizontalInput, verticalInput).normalized;
+
+        RotatePlayer();
     }
     private void FixedUpdate()
     {
-        // Move the Rigidbody
-        rb.velocity = movement * moveSpeed;
+        rb.velocity = movement * moveSpeed * 10 * Time.fixedDeltaTime;
+
+        //rb.velocity = Vector2.zero;
 
     }
 
-
-    void RotatePlayer(float horizontalInput, float verticalInput)
+    void GetPlayerInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+    void RotatePlayer()
     {
 
-        if(horizontalInput < 0)
+        if(Input.GetAxisRaw("Horizontal") < 0)
         {
             this.transform.eulerAngles = new Vector3(0, 0, 90);
         }
-        else if(horizontalInput > 0)
+        else if(Input.GetAxisRaw("Horizontal") > 0)
         {
             this.transform.eulerAngles = new Vector3(0, 0, -90);
         }
 
-        if(verticalInput < 0)
+        if(Input.GetAxisRaw("Vertical") < 0)
         {
             this.transform.eulerAngles = new Vector3(0, 0, 180);
         }
-        else if (verticalInput > 0)
+        else if (Input.GetAxisRaw("Vertical") > 0)
         {
             this.transform.eulerAngles = Vector3.zero;
         }
